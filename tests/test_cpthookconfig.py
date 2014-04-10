a31657f6a3f77b4cd93ff608d7bd774223dfbfb1
Alexander Else <alexander.else@team.telstra.com>
2014-04-10 16:43:27 +1000

    Add unit tests

---
+++ tests/test_cpthookconfig.py
@@ -0,0 +1,28 @@
+import os.path
+import sys
+import tempfile
+import unittest2 as unittest
+
+from cpthook import CptHookConfig
+import cpthook
+
+
+def cfgfile():
+    """Return name of config file corresponding with calling function name"""
+    fun_name = sys._getframe(1).f_code.co_name
+    return '/'.join((
+        os.path.dirname(os.path.realpath(__name__)),
+        'tests',
+        'configs',
+        fun_name + '.cfg'))
+
+
+class UnitTests(unittest.TestCase):
+
+    def test_nonexistent_config(self):
+        def f():
+            h = CptHookConfig('doesnotexist.cfg')
+        self.assertRaises(IOError, f)
+
+    def test_empty_repo_block(self):
+        h = CptHookConfig(cfgfile())
