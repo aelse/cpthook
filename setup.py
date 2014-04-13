b9956ca1be62dbfddee0fdcd5a0852993aca29fe
Alexander Else <alexander.else@team.telstra.com>
2014-04-14 09:58:30 +1000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import os.path
+__dir__ = os.path.dirname(os.path.abspath(__file__))
+
+setup(
+    name='cpthook',
+    license='BSD',
+    py_modules=['cpthook'],
+    scripts=['cpthook'],
+    version='0.0.1',
+    install_requires=[],
+
+    description='Centrally manage hooks for git repositories',
+    long_description=open(os.path.join(__dir__, 'README.md')).read(),
+
+    author='Alexander Else',
+    author_email='aelse@else.id.au',
+    url='https://github.com/aelse/cpthook',
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: Other/Proprietary License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Software Development",
+        "Topic :: System :: Systems Administration",
+    ]
+)
