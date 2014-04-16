65e5b17ececbe1ee4ce08c723a7bc50996bcca1a
Alexander Else <aelse@else.id.au>
2014-04-16 18:20:50 +1000

    Add docstrings for CptHook class

--- cpthook.py
+++ cpthook.py
@@ -286,13 +286,27 @@ class CptHookConfig(object):
 
 
 class CptHook(object):
+    """A git hook execution layer
+
+    CptHook provides a mechanism for running multiple hook scripts
+    for a particular hook type (eg. post-commit) for configured
+    repositories.
+
+    This class manages repository hooks, installing a wrapper that
+    executes the cpthook program to run each of the hook scripts
+    configured to be run for a hook type in a repository.
+
+    Configuration is managed through an ini-style file
+    (see CptHookConfig)"""
 
     def __init__(self, config_file):
+        """Called with the filename of a cpthook configuration file"""
         self.config_file = config_file
         self.config = CptHookConfig(config_file)
         self.dry_run = False
 
     def _script_name(self):
+        """Returns path and filename of executing python program"""
         import __main__ as main
         return os.path.abspath(os.path.realpath(main.__file__))
 
@@ -311,6 +325,10 @@ class CptHook(object):
             return True
 
     def add_hooks_to_repo(self, repo_path, hooks):
+        """Called with a path to a repository and a list of hooks
+
+        Creates a bash wrapper to run cpthook when git runs each hook"""
+
         template = (
             "#!/bin/sh\n"
             "#\n"
@@ -392,6 +410,8 @@ class CptHook(object):
         return None
 
     def install_hooks(self):
+        """Installs configured hooks into managed repositories"""
+
         for repo in self.config.repos():
             logging.debug('Examining repo {0}'.format(repo))
             repo_path = self._locate_repo(repo)
@@ -486,6 +506,18 @@ class CptHook(object):
             return True
 
     def run_hook(self, hook, args):
+        """Runs a given hook type (eg. post-commit)
+
+        Expects execution within the git repository (as git does).
+        Attempts to run each script of the given hook type that
+        is enabled for the current repository.
+
+        Execution halts when all scripts are run or earlier if
+        a hook script terminated with a non-zero exit code.
+
+        Returns 0, or the non-zero exit code from the script that
+        terminated with that exit code."""
+
         if not self._is_git_repo(os.path.curdir):
             logging.warn('{0} is not a git repo?'.format(
                 os.path.realpath(os.path.curdir)))
